02ea3d53c00f3c60c5da21c348faebbf945fd1b4
John Mager <jmageriii@gmail.com>
2019-03-31 10:22:13 -0400

    Imported keymap from ergodox configurator

---
+++ keyboards/ergodox_ez/keymaps/claymager/config.h
@@ -0,0 +1,13 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 12
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 5
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 5
+
